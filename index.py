583def075f7cf0af1364cdbcc5427da9669a7084
jyoti0346 <jyoti0346.be20@chitkara.edu.in>
2024-03-14 23:40:44 +0530

    python changes

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-print("Hii")
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Test the function
+number = int(input("Enter a number: "))
+result = even_odd(number)
+print("The number is", result)
